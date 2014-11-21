71484dc4f69681e7d7059eb60aa3341931a2e0c5
mbfrahry <mbfrahry@gmail.com>
2014-11-21 11:09:22 -0700

    tester

--- myresolver.cc
+++ myresolver.cc
@@ -381,18 +381,18 @@ char* ReadIPv6Address(unsigned char* reader,unsigned char*buffer, int*count)
 
     IP_addr = (char *)malloc(40);
 
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < 8; i++)
     {
       // mulitply the bytes together so that way you can get this to work 
       // and print out the ipv6 name 
         char str[9];
-        int num = (unsigned int)*reader;
-        printf("reader is: %d\n",*reader+i);
-        sprintf(str, "%d", num);
+        short num = (unsigned short)*reader;
+        printf("reader is: %02X\n",*reader+i);
+        sprintf(str, "%02X", num);
         //printf("%s", str);
         reader++;
         
-        if(i > 0 && i < 15 && i % 2 == 0)
+        if(i > 0 && i < 7 && i % 2 == 0)
         {
             printf(":");
         }
