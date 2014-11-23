9ac1e07b95bf1f6bf03d5fc6ffdcece3e5b8ffa5
jorsutton92 <jorsutton92@gmail.com>
2014-11-23 12:26:26 -0700

    Trying to read the RRSig packet

--- myresolver.cc
+++ myresolver.cc
@@ -286,15 +286,16 @@ using std::endl;
             printf("%01x\t",*dnsAnswerSection); //label 
             dnsAnswerSection++;
 
+
+
+            printf("%02x\t",ntohl(*((unsigned int*)dnsAnswerSection))); // original TTL
+            dnsAnswerSection+=4;
             printf("NEXT BYTES \n");
             for (int i = 0; i < 4; ++i)
             {
               printf("%02x\t",*dnsAnswerSection);
               dnsAnswerSection++;
             }
-
-            // printf("%02x\t",ntohl(*dnsAnswerSection)); // original TTL
-            // dnsAnswerSection+=4;
             // printf("%d\t",ntohl(*dnsAnswerSection) );
             // dnsAnswerSection+=4;
             // printf("%d\t",ntohl(*dnsAnswerSection) );
